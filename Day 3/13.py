1701f23ac6906c700c93a45a9ca620cbdb7de0a4
AyushBorad <85186413+AyushBorad@users.noreply.github.com>
2021-06-11 17:14:55 +0530

    Update 13.py

--- Day 3/13.py
+++ Day 3/13.py
@@ -3,8 +3,8 @@ n2=input("enter the second number :")
 n3=input("enter the third number :")
 
 if n1>n2 and n2>n3:
-    print("{} is the greates number".format(n1))
+    print("{} is the greatest number".format(n1))
 elif n2>n1 and n1>n3:
-    print("{} is the greates number".format(n2))
+    print("{} is the greatest number".format(n2))
 else:
-    print("{} is the greates number".format(n3))
+    print("{} is the greatest number".format(n3))
