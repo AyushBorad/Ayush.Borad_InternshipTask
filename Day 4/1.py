aacfa9ab1ceb842172aa92ca095f9da587f8655a
AyushBorad <85186413+AyushBorad@users.noreply.github.com>
2021-06-13 14:08:59 +0530

    Add files via upload

---
+++ Day 4/1.py
@@ -0,0 +1,6 @@
+# Defining a function. (with an argument)
+def myfn(name):
+    print("Hello.......")
+    print("My name is : ", name)
+
+myfn("Ayush ")
---
+++ Day 4/2.py
@@ -0,0 +1,6 @@
+#Example with return statement.
+def myfn(name):
+    return name
+
+name = myfn("Ayush")
+print("My name is ",name)
